ff8717460054ad8ee7dfe889f95e9e93f6200792
xieze <xieze333@163.com>
2016-10-19 11:02:53 +0800

    first commit

---
+++ assets/js/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2016/10/18.
+ */
+var created = {
+    administrator : "on",d:"a",
+    look : "xieze",zz:"zz"
+}
---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+/**
+ * Created by Administrator on 2016/10/18.
+ */
+// 载入外挂
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload');
+
+// 样式
+gulp.task('styles', function() {
+    return gulp.src('assets/less/test.less')
+        .pipe(less({
+            compress: true
+        }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// 脚本
+gulp.task('scripts', function() {
+    return gulp.src('assets/js/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// 图片
+gulp.task('images', function() {
+    return gulp.src('assets/assets/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(notify({ message: 'Images task complete' }));
+});
+
+// 清理
+gulp.task('clean', function() {
+    return gulp.src(['dist/css', 'dist/js', 'dist/images'], {read: false})
+        .pipe(clean());
+});
+
+// 预设任务
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+// 看守
+gulp.task('watch', function() {
+
+    // 看守所有.scss档
+    gulp.watch('assets/less/**/*.less', ['styles']);
+    gulp.watch('assets/css/**/*.css', ['styles']);
+
+    // 看守所有.js档
+    gulp.watch('assets/js/**/*.js', ['scripts']);
+
+    // 看守所有图片档
+    gulp.watch('assets/images/**/*', ['images']);
+
+    // 建立即时重整伺服器
+    var server = livereload();
+
+    // 看守所有位在 dist/  目录下的档案，一旦有更动，便进行重整
+    gulp.watch(['dist/**']).on('change', function(file) {
+        server.changed(file.path);
+    });
+
+});
